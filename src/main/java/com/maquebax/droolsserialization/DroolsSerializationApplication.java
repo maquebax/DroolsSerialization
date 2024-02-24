b820056deed0c8acb02d964aafb99b54354a1dc5
vignesh sridharan <vignesh.sridharan@navis.com>
2024-02-24 12:07:51 +0530

    Initial commit

---
+++ src/main/java/com/maquebax/droolsserialization/DroolsSerializationApplication.java
@@ -0,0 +1,13 @@
+package com.maquebax.droolsserialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsSerializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsSerializationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maquebax/droolsserialization/DroolsSerializationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maquebax.droolsserialization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsSerializationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
